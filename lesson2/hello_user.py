28347f95d484e4163919834f85ba91f40433abaf
ku4umid3e <kuchumid3e@gmail.com>
2021-09-08 23:16:43 +0300

    ДЗ по циклу while

---
+++ lesson2/hello_user.py
@@ -0,0 +1,27 @@
+def hellow_user():
+    while True:
+        question = input("Как дела?\n")
+        if question == "Хорошо":
+            break
+        else:
+            continue
+
+#hellow_user()
+
+question_answer = {
+    "Как дела": "Хорошо!",
+    "Что делаешь?": "Программирую",
+    }
+
+def ask_user(question_answer):
+    while True:
+        ask = input("Спроси меня 'Как дела' или 'Что делаешь?' скажи 'бай': \n")
+        if ask == 'бай':
+            break
+        else:
+            if question_answer.get(ask) == None:
+                continue
+            else:
+                print(question_answer.get(ask))
+
+ask_user(question_answer)
