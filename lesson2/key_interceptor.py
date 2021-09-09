ea71cd545ba9046053b3e1955084354d327d29fb
ku4umid3e <kuchumid3e@gmail.com>
2021-09-09 21:41:04 +0300

    ДЗ исключения

--- lesson2/key_interceptor.py
+++ lesson2/key_interceptor.py
@@ -10,3 +10,21 @@ def hellow_user():
         print('\nПока!')
 
 hellow_user()
+
+#Задание 2
+
+def discounted(price, discount, max_discount=20):
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+        if max_discount >= 100:
+            raise ValueError('Слишком большая максимальная скидка')
+        if discount >= max_discount:
+            print(price)
+        else:
+            print(price - (price * discount / 100))
+    except(ValueError, TypeError):
+        print('ОШИБКА!')
+
+discounted("10000", "15", 20.5)
